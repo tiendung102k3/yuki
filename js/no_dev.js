89edadd76aac42f3a73e2709fcfa991a6dfc6656
Thienan1092005 <ogyminecraft497@gmail.com>
2024-03-17 14:33:18 +0700

    devcc

--- js/no_dev.js
+++ js/no_dev.js
@@ -1,38 +1,38 @@
-// // Can't inspect element, contextmenu
-// let alertCount = 0;
-// document.addEventListener("contextmenu", (e) => {
-//   e.preventDefault();
-//   if (alertCount === 0) {
-//     alert("nghịch cái gì đấy eam ?");
-//   } else if (alertCount === 1) {
-//     alert("ủa nói rồi mà cứ làm hỉ ?? ?");
-//   } else if (alertCount === 2) {
-//     alert("con  người chứ  phải con cak đâu mÀ  nói  hoài không nghe thế ?");
-//   }
-//   alertCount++;
-// });
+// Can't inspect element, contextmenu
+let alertCount = 0;
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  if (alertCount === 0) {
+    alert("nghịch cái gì đấy eam ?");
+  } else if (alertCount === 1) {
+    alert("ủa nói rồi mà cứ làm hỉ ?? ?");
+  } else if (alertCount === 2) {
+    alert("con  người chứ  phải con cak đâu mÀ  nói  hoài không nghe thế ?");
+  }
+  alertCount++;
+});
 
-// function ctrlShiftKey(e, keyCode) {
-//   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-// }
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
 
-// // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + Ut
-// document.onkeydown = (e) => {
-//   if (
-//     event.keyCode === 123 ||
-//     ctrlShiftKey(e, "I") ||
-//     ctrlShiftKey(e, "J") ||
-//     ctrlShiftKey(e, "C") ||
-//     (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-//   ) {
-//     e.preventDefault();
-//     if (alertCount === 0) {
-//       alert("nghịch cái gì đấy eam ?");
-//     } else if (alertCount === 1) {
-//       alert("ủa nói rồi mà cứ làm hỉ ?? ?");
-//     } else if (alertCount === 2) {
-//       alert("con  người chứ  phải con cak đâu mÀ  nói  hoài không nghe thế ?");
-//     }
-//     alertCount++;
-//   }
-// };
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + Ut
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, "I") ||
+    ctrlShiftKey(e, "J") ||
+    ctrlShiftKey(e, "C") ||
+    (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+  ) {
+    e.preventDefault();
+    if (alertCount === 0) {
+      alert("nghịch cái gì đấy eam ?");
+    } else if (alertCount === 1) {
+      alert("ủa nói rồi mà cứ làm hỉ ?? ?");
+    } else if (alertCount === 2) {
+      alert("con  người chứ  phải con cak đâu mÀ  nói  hoài không nghe thế ?");
+    }
+    alertCount++;
+  }
+};
