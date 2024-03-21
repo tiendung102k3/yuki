60caca8aeef46fa24eb8982da52508ba5db0d98d
Thienan1092005 <ogyminecraft497@gmail.com>
2024-03-21 19:02:46 +0700

    tich xanh

--- card-discord/main.js
+++ card-discord/main.js
@@ -7,7 +7,7 @@ for (let i = 0; i < roleCode.length; i++) {
   console.log(`active${indexNum}`);
 }
 
-const proflieId = "654675180529909789"; // dán proflieid của bạn  vô đây nha //
+const proflieId = "854959889322213406"; // dán proflieid của bạn  vô đây nha //
 let userData = null;
 async function fetchData() {
   try {
